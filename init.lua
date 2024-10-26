f74412b0010bfdd1dfc98546a60d3bb1310d3f7e
Fredrik Hult <fredrik.hult@outlook.com>
2024-10-26 21:47:57 +0200

    Added GLSL highlighting

--- init.lua
+++ init.lua
@@ -1017,6 +1017,9 @@ require('lazy').setup({
       require('lsp_signature').setup()
     end,
   },
+  {
+    'tikhomirov/vim-glsl',
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
