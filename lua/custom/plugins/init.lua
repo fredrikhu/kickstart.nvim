d06ca48cf2a03bacf2659df2f563db022310f2a1
Fredrik Hult <fredrik@hult.email>
2026-03-02 22:35:08 +0100

    Add org mode

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,32 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'nvim-orgmode/orgmode',
+    event = 'VeryLazy',
+    ft = { 'org' },
+    config = function()
+      require('orgmode').setup {
+        org_agenda_files = '~/org/**/*',
+        org_default_notes_file = '~/org/refile.org',
+      }
+    end,
+  },
+  {
+    'akinsho/org-bullets.nvim',
+    ft = { 'org' },
+    config = function()
+      require('org-bullets').setup {
+        symbols = {
+          headlines = { '◉', '○', '✸', '✿' },
+          checkboxes = {
+            half = { '', 'OrgTSCheckboxHalfChecked' },
+            done = { '✓', 'OrgDone' },
+            todo = { '˙', 'OrgTODO' },
+          },
+        },
+      }
+    end,
+  },
+}
